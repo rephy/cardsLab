1afb6c11576f46f6f12fba812d4e34dc41ed5117
Raphael Manayon <rapmanayon@gmail.com>
2026-05-22 17:02:49 -0700

    Activity 2 -'forgot to add files'

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Deck {
+    private ArrayList<Card> deck;
+    private ArrayList<Card> dealt;
+    private int size;
+
+    public Deck(String[] ranks, String[] suits, int[] pointValues) {
+        int minLengthOne = Math.min(ranks.length, suits.length);
+        int minLengthTwo = Math.min(pointValues.length, suits.length);
+        int minLength = Math.min(minLengthOne, minLengthTwo);
+
+        deck = new ArrayList<>();
+
+        for (int i = 0; i < minLength; i++) {
+            deck.add(new Card(ranks[i], suits[i], pointValues[i]));
+            size++;
+        }
+
+        dealt = new ArrayList<>();
+
+        shuffle();
+    }
+
+    public void shuffle() {
+        return;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Card deal() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        Card dealtCard = deck.remove(size - 1);
+
+        dealt.add(dealtCard);
+        size--;
+
+        return dealtCard;
+    }
 }
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class DeckTester {
+    public static void main(String[] args) {
+        Deck deckOne = new Deck(new String[]{"Ace", "Five", "Queen"}, new String[]{"Hearts", "Spades", "Diamonds"}, new int[]{11, 5, 10});
+        Deck deckTwo = new Deck(new String[]{"Six", "Three", "Jack"}, new String[]{"Spades", "Spades", "Clubs"}, new int[]{6, 3, 10});
+        Deck deckThree = new Deck(new String[]{"Two", "Two", "Ace"}, new String[]{"Diamonds", "Clubs", "Hearts"}, new int[]{2, 2, 1});
+
+        int minLengthOne = Math.min(deckOne.size(), deckTwo.size());
+        int minLengthTwo = Math.min(deckOne.size(), deckThree.size());
+        int minLength = Math.min(minLengthOne, minLengthTwo);
+
+        for (int i = 0; i < minLength; i++) {
+            System.out.println(deckOne.size() + ", " + deckTwo.size() + ", " + deckThree.size());
+            System.out.println(deckOne.deal() + ", " + deckTwo.deal() + ", " + deckThree.deal());
+
+            System.out.println();
+        }
+    }
 }
