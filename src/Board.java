a9c0cc61f329560081238eca833f3a05dd7bf38e
Raphael Manayon <rapmanayon@gmail.com>
2026-05-26 06:24:17 -0700

    Activity 8

---
+++ src/Board.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public abstract class Board {
+    private Deck deck;
+    private Card[] cards;
+
+    public Board(String[] ranks, String[] suits, int[] values, int boardSize) {
+        deck = new Deck(ranks, suits, values);
+
+        cards = new Card[boardSize];
+    }
+
+    public void newGame() {
+        deck.shuffle();
+        dealMyCards();
+    }
+
+    public int size() {
+        return cards.length;
+    }
+
+    public int deckSize() {
+        return deck.size();
+    }
+
+    public void deal(int k) {
+        cards[k] = deck.deal();
+    }
+
+    public void dealMyCards() {
+        for (int i = 0; i < size(); i++) {
+            deal(i);
+        }
+    }
+
+    public Card cardAt(int k) {
+        return cards[k];
+    }
+
+    public abstract boolean isLegal(int[] indexes);
+
+    public abstract boolean anotherPlayIsPossible();
+
+    public int[] cardIndexes() {
+        ArrayList<Integer> listIndexes = new ArrayList<>();
+
+        for (int i = 0; i < cards.length; i++) {
+            if (cards[i] != null) {
+                listIndexes.add(i);
+            }
+        }
+
+        int size = listIndexes.size();
+        int[] indexes = new int[size];
+        for (int i = 0; i < size; i++) {
+            indexes[i] = listIndexes.get(i);
+        }
+
+        return indexes;
+    }
+
+    public String toString() {
+        String s = "";
+
+        for (int k = 0; k < cards.length; k++) {
+            s += k + ": " + cards[k] + "\n";
+        }
+
+        return s;
+    }
+
+    public boolean isEmpty() {
+        for (Card card : cards) {
+            if (card != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean gameIsWon() {
+        return isEmpty() && deck.isEmpty();
+    }
+
+    private void replaceSelectedCards(int[] indexes) {
+        for (int i : indexes) {
+            deal(i);
+        }
+    }
+}
--- src/ElevensBoard.java
+++ src/ElevensBoard.java
@@ -1,44 +1,10 @@
 import java.util.ArrayList;
 
-public class ElevensBoard {
-    private Deck deck;
-    private Card[] cards;
-
+public class ElevensBoard extends Board {
     public ElevensBoard() {
-        String[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-        String[] suits = {"Diamonds", "Hearts", "Spades", "Clubs"};
-        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
-
-        deck = new Deck(ranks, suits, values);
-
-        cards = new Card[9];
-    }
-
-    public void newGame() {
-        deck.shuffle();
-        dealMyCards();
-    }
-
-    public int size() {
-        return cards.length;
-    }
-
-    public int deckSize() {
-        return deck.size();
-    }
-
-    public void deal(int k) {
-        cards[k] = deck.deal();
-    }
-
-    public void dealMyCards() {
-        for (int i = 0; i < 9; i++) {
-            deal(i);
-        }
-    }
-
-    public Card cardAt(int k) {
-        return cards[k];
+        super(new String[]{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"},
+                new String[]{"Diamonds", "Hearts", "Spades", "Clubs"},
+                new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0}, 9);
     }
 
     public boolean isLegal(int[] indexes) {
@@ -58,7 +24,7 @@ public class ElevensBoard {
     public boolean containsPairSum11(int[] indexes) {
         Card[] selectedCards = new Card[indexes.length];
         for (int i = 0; i < indexes.length; i++) {
-            selectedCards[i] = cards[indexes[i]];
+            selectedCards[i] = cardAt(indexes[i]);
         }
 
         for (int i = 0; i < selectedCards.length; i++) {
@@ -79,7 +45,7 @@ public class ElevensBoard {
 
         Card[] selectedCards = new Card[indexes.length];
         for (int i = 0; i < indexes.length; i++) {
-            selectedCards[i] = cards[indexes[i]];
+            selectedCards[i] = cardAt(indexes[i]);
         }
 
         for (Card card : selectedCards) {
@@ -97,53 +63,5 @@ public class ElevensBoard {
         }
 
         return false;
-    }
-
-    public int[] cardIndexes() {
-        ArrayList<Integer> listIndexes = new ArrayList<>();
-
-        for (int i = 0; i < cards.length; i++) {
-            if (cards[i] != null) {
-                listIndexes.add(i);
-            }
-        }
-
-        int size = listIndexes.size();
-        int[] indexes = new int[size];
-        for (int i = 0; i < size; i++) {
-            indexes[i] = listIndexes.get(i);
-        }
-
-        return indexes;
-    }
-
-    public String toString() {
-        String s = "";
-
-        for (int k = 0; k < cards.length; k++) {
-            s += k + ": " + cards[k] + "\n";
-        }
-
-        return s;
-    }
-
-    public boolean isEmpty() {
-        for (Card card : cards) {
-            if (card != null) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public boolean gameIsWon() {
-        return isEmpty() && deck.isEmpty();
-    }
-
-    private void replaceSelectedCards(int[] indexes) {
-        for (int i : indexes) {
-            deal(i);
-        }
     }
 }
